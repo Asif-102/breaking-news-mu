2f8cb7875a8b59d718d59e8de981da14b7f0ce68
Asif2535 <aiqbal517@gmail.com>
2021-03-03 19:32:01 +0600

    know about axios

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import { Button } from '@material-ui/core';
 import News from './components/News/News';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [articles,setArticles] = useState([]);
+
+  // useEffect(()=>{
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=f39ee1f6f38e49819a31260f6006ae0e';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => setArticles(data.articles))
+  // },[])
+
+
   useEffect(()=>{
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=f39ee1f6f38e49819a31260f6006ae0e';
-    fetch(url)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles))
+    axios(url)
+    .then(data => setArticles(data.data.articles))
   },[])
+
   return (
     <div>
       <h2>Headlines: {articles.length}</h2>
